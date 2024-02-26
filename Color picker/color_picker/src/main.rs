8f1b266176aa85f5127e8719c3635bcef71ee354
solaire-pc <contact@unharmfulhazard.com>
2024-02-26 15:18:00 -0300

    Updated the project

--- Color picker/color_picker/src/main.rs
+++ Color picker/color_picker/src/main.rs
@@ -1,16 +1,23 @@
-use enigo;
+use enigo::Enigo;
 use enigo::MouseControllable;
-use screenshot;
-use image::{GenericImageView, Rgba};
+use screenshot_rs::screenshot_full;
+use std::thread::sleep;
+use std::time::Duration;
+use image;
 
-fn get_rgb_value(x: u32, y: u32) -> Option<Rgba<u8>> {
-    let screenshot = screenshot::Screenshot::get_pixel(x, y);
+fn get_rgb(x: i32, y: i32) {
+    let path = String::from("/home/solaire/RustroverProjects/rust-studies/Color picker/color_picker/screenshot.png");
+    screenshot_full(path);
+
+    let pixel = image::GenericImageView::get_pixel(path, x as u32, y as u32);
 }
 
 fn main() {
-    let enigo = enigo::Enigo::new();
+    let enigo = Enigo::new();
     while true {
         let (x, y) = enigo.mouse_location();
         println!("x: {x} | y: {y}");
+        get_rgb(x, y);
+        sleep(Duration::from_secs(2));
     }
 }
