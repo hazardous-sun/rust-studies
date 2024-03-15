ddd77127e19f2782ff32dbeaed945d4be339b775
solaire-notebook <contact@unharmfulhazard.com>
2024-03-14 23:58:07 -0300

    Finished section 12.3

--- The Rust Programming Language/12. An IO Project Building a Command Line Program/12.3 Refactoring to Improve Modularity and Error Handling/minigrep/src/main.rs
+++ The Rust Programming Language/12. An IO Project Building a Command Line Program/12.3 Refactoring to Improve Modularity and Error Handling/minigrep/src/main.rs
@@ -1,36 +1,23 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
+    // --snip--
+
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    // --snip--
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-        // --snip--
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config { query, file_path }
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
